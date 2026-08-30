3ec6cc9ef25bee67ee16a1f7d210c06b8a2ce50b
mrshadow0304 <mr.shadow0304@gmail.com>
2026-08-30 22:01:19 +0530

    Update findoc.js

--- home/Find_doctors/findoc.js
+++ home/Find_doctors/findoc.js
@@ -50,7 +50,7 @@ const baselineDoctors = [
         specialization: "Dermatology",
         experience: 9,
         hospital: { name: "KIMS Hospitals", location: "Secunderabad, Hyderabad" },
-        profileImageURL: "https://images.unsplash.com/photo-1594824813566-78a08862319d?q=80&w=400",
+        profileImageURL: "https://i.pinimg.com/736x/c5/a3/90/c5a3904b38eb241dd03dd30889599dc4.jpg",
         phone: "+91 9876543212"
     },
     {
